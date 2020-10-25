779f1edee5ce0a27d147189cd9e2771beccf930e
Evgenia Katsada <evgkatsada@gmail.com>
2020-10-25 13:24:16 +0200

    Lecture 6: findAny()

--- src/test/java/lectures/Lecture6.java
+++ src/test/java/lectures/Lecture6.java
@@ -13,7 +13,8 @@ public class Lecture6 {
   @Test
   public void findAny() throws Exception {
     Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
+    int any = Arrays.stream(numbers).filter(n -> n < 10).findAny().orElse(0);
+    System.out.println(any);
   }
 
   @Test
